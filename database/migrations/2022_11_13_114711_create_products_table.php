3e64d215a7351a08ba2012b496c85572e1f8b990
Faza01 <91936066+Faza01@users.noreply.github.com>
2022-12-04 20:18:42 +0700

    transaction

--- database/migrations/2022_11_13_114711_create_products_table.php
+++ database/migrations/2022_11_13_114711_create_products_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('category_id')->constrained('categories')->onUpdate('cascade');
+            $table->foreignId('category_id')->constrained('categories');
             $table->string('category');
             $table->string('description');
             $table->integer('price');
